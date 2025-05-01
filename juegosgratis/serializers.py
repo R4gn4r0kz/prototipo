bef40fab06c8eaf86fcb4021b8b84f40a5e6b2ab
R4gn4r0kz <diegotomas124@gmail.com>
2025-05-01 18:26:57 -0400

    Delete juegosgratis directory

--- juegosgratis/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Juego
-
-class JuegoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Juego
-        fields = '__all__'
--- juegosgratis/urls.py
+++
@@ -1,9 +0,0 @@
-# juegosgratis/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('juegos-gratis/', views.juegos_list, name='juegos_list'),  # HTML
-    path('api/juegos-gratis/', views.JuegosGratisView.as_view(), name='juegos_api'),  # API JSON
-]
-
