4a4727255e65fe2c3e30bef9927ecdf93390c68b
R4gn4r0kz <diegotomas124@gmail.com>
2025-05-01 18:27:37 -0400

    Delete virtualgames directory

--- virtualgames/views.py
+++
@@ -1,8 +0,0 @@
-import requests
-from django.shortcuts import render
-
-def mostrar_juegos(request):
-    url = 'http://localhost:8000/api/juegos-gratis/'
-    response = requests.get(url)  # Hacer una solicitud GET a la API
-    juegos = response.json()  # Obtener los datos en formato JSON
-    return render(request, 'juegosgratis/juegos_list.html', {'juegos': juegos})
