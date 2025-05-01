c36fabf7c8593536f320a04727837564ae8c6176
R4gn4r0kz <diegotomas124@gmail.com>
2025-05-01 18:27:28 -0400

    Delete vgamescore directory

--- vgamescore/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Videojuego
-
-class VideojuegoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Videojuego
-        fields = ['id', 'nombre', 'descripcion', 'precio', 'fecha_lanzamiento', 'genero', 'stock']
